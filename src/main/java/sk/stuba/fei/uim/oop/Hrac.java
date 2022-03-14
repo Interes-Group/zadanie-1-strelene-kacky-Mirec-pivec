4913f227da43710fa43605c4bb51bc6148daee89
Mirko <MirkoGimi@gmail.com>
2022-03-14 14:40:35 +0100

    konstruktor Hraca

--- src/main/java/sk/stuba/fei/uim/oop/Hrac.java
+++ src/main/java/sk/stuba/fei/uim/oop/Hrac.java
@@ -1,6 +1,13 @@
 package sk.stuba.fei.uim.oop;
 
+import sk.stuba.fei.uim.oop.utility.ZKlavesnice;
+
 public class Hrac {
     private String meno;
     private int zivoty;
+
+    public Hrac(){
+        meno = ZKlavesnice.readString("zadaj meno hraca");
+        zivoty = 5;
+    };
 }
